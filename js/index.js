d6ab3369f4beef0146c2937d97f2e516d0884ddf
Ella G <elviraangelinagraf@gmail.com>
2024-02-22 17:20:40 +0100

    Added icons, js and other new things

---
+++ js/index.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  function seeEmail(event) {
+    event.preventDefault();
+    let email = "elvira.a.graf@gmail.com";
+    alert("My 📧 address is: " + email);
+  }
+
+  let emailIcon = document.querySelector("#emailIcon");
+  emailIcon.addEventListener("click", seeEmail);
+});
