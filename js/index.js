20cde2bbbfe5b72d0f5a59def882830bf6941df1
Ella G <elviraangelinagraf@gmail.com>
2024-02-24 20:28:54 +0100

    Corrections and Changes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
+  console.log("Index script DOMContentLoaded");
+
   function seeEmail(event) {
     event.preventDefault();
     let email = "elvira.a.graf@gmail.com";
@@ -7,4 +9,35 @@ document.addEventListener("DOMContentLoaded", function () {
 
   let emailIcon = document.querySelector("#emailIcon");
   emailIcon.addEventListener("click", seeEmail);
+
+  function toggleLanguage() {
+    let languageElement = document.querySelector(".switch > span.on");
+
+    if (languageElement.textContent === "EN") {
+      languageElement.textContent = "DE";
+    } else {
+      languageElement.textContent = "EN";
+    }
+
+    translatePage(languageElement.textContent);
+  }
+
+  function translatePage(language) {
+    const elementsToTranslate = document.querySelectorAll("[data-translate]");
+
+    elementsToTranslate.forEach((element) => {
+      const translationKey = element.getAttribute("data-translate");
+
+      if (
+        translations[translationKey] &&
+        translations[translationKey][language]
+      ) {
+        element.textContent = translations[translationKey][language];
+      }
+    });
+  }
+
+  let switchElement = document.getElementById("language-toggle");
+
+  switchElement.addEventListener("change", toggleLanguage);
 });
---
+++ js/translation.js
@@ -0,0 +1,60 @@
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Translation script loaded");
+});
+
+const translations = {
+  "Portfolio of Ella Graf | Navigating the intersection of technology and design":
+    {
+      EN: "Portfolio of Ella Graf | Navigating the intersection of technology and design",
+      DE: "Portfolio von Ella Graf | Navigation an der Schnittstelle von Technologie und Design",
+    },
+  "Hello, my name is": {
+    EN: "Hello, my name is",
+    DE: "Hallo, mein Name ist",
+  },
+  "Frontend Developer, based in Swiss.": {
+    EN: "Frontend Developer, based in Switzerland.",
+    DE: "Frontend Entwicklerin aus der Schweiz.",
+  },
+  "Get started": {
+    EN: "Get started",
+    DE: "Loslegen",
+  },
+  "World-Clocks": {
+    EN: "World-Clocks",
+    DE: "Weltuhren",
+  },
+  "Observes the times around the world": {
+    EN: "Observes the times around the world",
+    DE: "Beobachtet die Zeiten rund um die Welt",
+  },
+  "View the project": {
+    EN: "View the project",
+    DE: "Projekt ansehen",
+  },
+  "Rhyme-Alchemist": {
+    EN: "Rhyme-Alchemist",
+    DE: "Reim-Alchemist",
+  },
+  "Elevate Your Words: Rhyme-Alchemist, Your Creative Companion for Instant French Poetry.":
+    {
+      EN: "Elevate Your Words: Rhyme-Alchemist, Your Creative Companion for beautiful French Poetry.",
+      DE: "Erhebe deine Worte: Reim-Alchemist, dein kreativer Begleiter für schöne französische Poesie.",
+    },
+  "Weather-App": {
+    EN: "Weather-App",
+    DE: "Wetter-App",
+  },
+  "Discover Weather Bliss: A Minimalist and User-Friendly Weather App": {
+    EN: "Discover Weather Bliss: A Minimalist and User-Friendly Weather App",
+    DE: "Entdecken Sie Weather Bliss: Eine minimalistische und benutzerfreundliche Wetter-App",
+  },
+  "Piano passion": {
+    EN: "Piano passion",
+    DE: "Piano Passion",
+  },
+  "Harmony Unleashed: A Musical Journey through the Piano's Past and Present": {
+    EN: "Harmony Unleashed: A Musical Journey through the Piano's Past and Present",
+    DE: "Harmony Unleashed: Eine musikalische Reise durch die Vergangenheit und Gegenwart des Klaviers",
+  },
+};
